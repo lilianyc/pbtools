ed53f96c7357040538373058aebf29af754bb0f0
lilianyc <lilianyangcrosson@gmail.com>
2019-09-16 03:15:26 +0200

    Get probability matrix from PBs

--- src/pbtools.py
+++ src/pbtools.py
@@ -5,6 +5,7 @@
 
 from pathlib import Path
 
+import pandas as pd
 import pbxplore as pbx
 
 #print(globals().get("__file__"))
@@ -14,16 +15,24 @@ import pbxplore as pbx
 trajectory = "md.trr"
 topology = "md.gro"
 
+sequences = []
 try:
     for chain_name, chain in pbx.chains_from_trajectory(trajectory, topology):
         dihedrals = chain.get_phi_psi_angles()
         pb_seq = pbx.assign(dihedrals)
-    
-    print(chain_name)
-    print(pb_seq)
+        sequences.append(pb_seq)
 except Exception as exc:
     print("Error", exc)
 
+# Count matrix with one row per sequence and one column per PB.
+# The readthedocs documentation has the 2 confused.
+# !!!: Duplicate sequences seem grouped together.
+count_matrix = pbx.analysis.count_matrix(sequences)
 
+count_matrix.shape
 
-
+df = pd.DataFrame(count_matrix)
+# Probability matrix.
+proba_df = df / len(sequences)
+# See if all probabilities are < 1.
+assert (proba_df < 1).all().all()
