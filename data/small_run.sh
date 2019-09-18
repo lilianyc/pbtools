6ae9dfd8ac62621c4f96b1a39bba97b59f6522eb
lilianyc <lilianyangcrosson@gmail.com>
2019-09-18 17:33:55 +0200

    Add documentation on network manipulation
    Added small script to get smaller datasets (faster to use)

---
+++ data/small_run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Get smaller demo files.
+
+wget https://raw.githubusercontent.com/pierrepo/PBxplore/master/demo_doc/psi_md_traj.gro
+wget https://raw.githubusercontent.com/pierrepo/PBxplore/master/demo_doc/psi_md_traj.xtc
