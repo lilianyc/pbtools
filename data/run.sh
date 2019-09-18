28b71976a84c3f67937c77cc55b0abb93862a0c9
lilianyc <lilianyangcrosson@gmail.com>
2019-09-18 11:55:18 +0200

    Add script to retrieve data

---
+++ data/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+wget -o- http://www.dsimb.inserm.fr/~tmp/DM_Calf-1_WT_production_long1.tar.gz
+tar -xvzf DM_Calf-1_WT_production_long1.tar.gz
+mv home/glaciere/benkafouf/goguet/Bureau/DM_Calf-1_WT/production_long1/md.trr .
+mv home/glaciere/benkafouf/goguet/Bureau/DM_Calf-1_WT/production_long1/md.gro .
